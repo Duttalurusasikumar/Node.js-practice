b1b2b36ff5e17082d54ab8bc9f3320f516f741f3
Duttaluru sasikumar <sasi54563@gmail.com>
2022-04-07 19:45:54 +0530

    Update and rename demo_fs_update.js to appendFile()_fs_update.js

--- Update Files/appendFile()_fs_update.js
+++ Update Files/appendFile()_fs_update.js
@@ -4,3 +4,5 @@ fs.appendFile('mynewfile1.txt', ' This is my text.', function (err) {
   if (err) throw err;
   console.log('Updated!');
 });
+
+//Append "This is my text." to the end of the file "mynewfile1.txt"
